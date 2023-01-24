4d5a95b04a769d6beaafd83d2f375e3e3aacc950
abodian <ajnbodian@gmail.com>
2023-01-24 15:13:51 +0000

    thermostat jest js exercise

---
+++ thermostat.js
@@ -0,0 +1,46 @@
+class Thermostat {
+  constructor() {
+    this.currentTemperature = 20;
+    this.powerSavingMode = true;
+  }
+
+  getTemperature() {
+    return this.currentTemperature;
+  }
+
+  up() {
+    if (this.currentTemperature >= 32 && this.powerSavingMode == false) {
+      return;
+    } else if (this.currentTemperature >= 25 && this.powerSavingMode == true) {
+      return;
+    }
+    this.currentTemperature += 1;
+  }
+
+  down() {
+    if (this.currentTemperature <= 10) {
+      return;
+    }
+    this.currentTemperature -= 1;
+  }
+
+  setPowerSavingMode(boolean) {
+    if (boolean === true) {
+      this.powerSavingMode = true;
+      if (this.currentTemperature > 25) {
+        this.currentTemperature = 25;
+      }
+      return "PSM is now on, max temperature is 25";
+    } else if (boolean === false) {
+      this.powerSavingMode = false;
+      return "PSM is now off, max temperature is 32";
+    } else return "Incorrect argument given, try again";
+  }
+
+  reset() {
+    this.currentTemperature = 20;
+    this.powerSavingMode = true;
+  }
+}
+
+module.exports = Thermostat;
---
+++ thermostat.test.js
@@ -0,0 +1,68 @@
+const Thermostat = require("./Thermostat");
+
+describe("Thermostat class", () => {
+  let thermostat = new Thermostat();
+
+  it("returns the current thermostat temperature", () => {
+    expect(thermostat.getTemperature()).toBe(20);
+  });
+
+  it("increases the thermo temp by 1 celcius", () => {
+    thermostat.up();
+    expect(thermostat.getTemperature()).toBe(21);
+  });
+
+  it("increases the thermo temp by 2 celcius", () => {
+    thermostat.up();
+    expect(thermostat.getTemperature()).toBe(22);
+  });
+
+  it("sets power saving mode as off", () => {
+    expect(thermostat.setPowerSavingMode(false)).toBe(
+      "PSM is now off, max temperature is 32"
+    );
+  });
+
+  it("increases temperature to 32 celcius max as PSM is off", () => {
+    for (let i = 0; i < 20; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toBe(32);
+  });
+
+  it("sets power saving mode as on", () => {
+    expect(thermostat.setPowerSavingMode(true)).toBe(
+      "PSM is now on, max temperature is 25"
+    );
+  });
+
+  it("increases temperature to 25 celcius max as PSM is on", () => {
+    for (let i = 0; i < 20; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toBe(25);
+  });
+
+  it("increases temperature to 26 celcius with PSM off", () => {
+    thermostat.setPowerSavingMode(false);
+    thermostat.up();
+    expect(thermostat.getTemperature()).toBe(26);
+  });
+
+  it("resets temperature to 20", () => {
+    thermostat.reset();
+    expect(thermostat.getTemperature()).toBe(20);
+  });
+
+  it("reduces temperature by 1 celsius", () => {
+    thermostat.down();
+    expect(thermostat.getTemperature()).toBe(19);
+  });
+
+  it("reduces temperature but stops at minimum temp of 10", () => {
+    for (let i = 0; i < 20; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getTemperature()).toBe(10);
+  });
+});
